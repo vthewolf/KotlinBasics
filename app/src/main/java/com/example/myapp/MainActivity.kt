95889c225cef103040a769bf8bb186c827bceb21
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2023-02-13 17:21:46 +0100

    Funciones
    Finalizado video de funciones con un archivo nuevo test.kt con una función y un ejercicio resuelto en la MainActivity de funciones con un toast

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
+        toast("Hello World")
+    }
+
+    private fun toast(message: String){
+        return Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/example/myapp/test.kt
@@ -0,0 +1,8 @@
+package com.example.myapp
+
+import android.widget.Toast
+
+// Función con formato de expresión.
+// Se sustituyen las llaves por el = al ser solo una expresión.
+fun add(x: Int, y: Int): Int = x + y
+
